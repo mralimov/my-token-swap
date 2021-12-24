64c483664c89fb61a52ade1b0a1d613e02ae6fae
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-24 09:46:56 -0500

    add index.html in webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+require('file-loader?name=[name].[ext]!./index.html');
 const appElement = document.getElementById('app');
 
 ReactDOM.render(<App />, appElement);
