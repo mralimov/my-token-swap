3b5cf2e48839713b84e988da7ed607ceb08acb0a
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-24 09:55:11 -0500

    App component styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import App from './App.scss';
 const App = () => {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+require('file-loader?name=[name].[ext]!./index.html');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-require('file-loader?name=[name].[ext]!./index.html');
+import './App.scss';
+
 const appElement = document.getElementById('app');
 
 ReactDOM.render(<App />, appElement);
