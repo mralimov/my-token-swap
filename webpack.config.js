323ecbb127f3282ccd9bc8c7571dcbc16f65b16f
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-24 10:19:52 -0500

    minicssextractplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   output: {
@@ -7,7 +8,7 @@ module.exports = {
   },
   devServer: {
     port: 3663,
-    watchContentBase: true,
+    hot: true,
   },
   module: {
     rules: [
@@ -20,8 +21,9 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
