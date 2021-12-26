d33424b9ee055a830c225253e13246e80d7a52ec
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-26 13:38:23 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.scss';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello Donnie's Swapping web app</h1>
+      <h1>My uniswap</h1>
     </div>
   );
 };
