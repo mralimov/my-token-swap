874f29d8232eaec7a33418f08aff66fe39ac82e5
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-28 13:39:51 -0500

    structuring swapbox component

--- src/components/SwapBox/SwapBox.jsx
+++ src/components/SwapBox/SwapBox.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { BiSortAlt2, BiCog, BiChevronDown } from 'react-icons/bi';
 
 const SwapBox = () => {
   return (
-    <div>
-      <h1>SwapBox</h1>
+    <div className='swap'>
+      <div className='swap_container'>
+        <div className='swap_container_head'>
+          <div className='swap_container_head--wrap'>
+            <div className='swap_container_head_title'>Swap</div>
+
+            <button className='swap_settings_button'>
+              <BiCog />
+            </button>
+            {/* <div className='swap_settings'></div> */}
+          </div>
+        </div>
+        <div className='swap_currency_from'>
+          <div className='swap_currency_from_wrap'>
+            <input></input>
+            <button>
+              <img></img>
+              <span></span>
+              <BiChevronDown />
+            </button>
+          </div>
+        </div>
+        <div className='swap_currency_switch'>
+          <BiSortAlt2 />
+        </div>
+        <div className='swap_currency_to'>
+          <div>
+            <input></input>
+            <button>
+              <button>
+                <img></img>
+                <span></span>
+                <BiChevronDown />
+              </button>
+            </button>
+          </div>
+        </div>
+        <div className='swap_connect_wallet'>
+          <button>Connect Wallet</button>
+        </div>
+      </div>
     </div>
   );
 };
