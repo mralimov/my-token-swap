06f433e71990c076a5235a8fea9996ed4ce1465d
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-29 16:03:19 -0500

    Select token modal window structure

---
+++ src/components/SwapBox/select-token/SelectToken.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+const SelectToken = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <div></div>
+              <svg></svg>
+            </div>
+            <div>
+              <input></input>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+                <div>
+                  <img></img>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div></div>
+          <div>
+            <div className='overflow'>
+              <div>
+                <div>
+                  <div>
+                    <div className='token_card_each_token_list'>
+                      <img></img>
+                      <div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </div>
+                    <div className='token_card_each_token_list'></div>
+                    <div className='token_card_each_token_list'></div>
+                    <div className='token_card_each_token_list'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='resize_trigger'></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectToken;
