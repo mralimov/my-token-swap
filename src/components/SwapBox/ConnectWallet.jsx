9ee7f2996d6dffb5fa6834e449e7b017db7e1d6b
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-29 12:07:27 -0500

    ConnectWallet component

---
+++ src/components/SwapBox/ConnectWallet.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConnectWallet = () => {
+  return (
+    <div className='swap_connect_wallet'>
+      <button>Connect Wallet</button>
+    </div>
+  );
+};
+
+export default ConnectWallet;
