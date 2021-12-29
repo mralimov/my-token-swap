c9497997101608eecabee0835e18a827ce4253c0
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-29 12:08:24 -0500

    brake SwapBox to smaller component

--- src/components/SwapBox/SwapBox.jsx
+++ src/components/SwapBox/SwapBox.jsx
@@ -1,48 +1,22 @@
 import React from 'react';
-import { BiSortAlt2, BiCog, BiChevronDown } from 'react-icons/bi';
+import { BiSortAlt2 } from 'react-icons/bi';
+
+import ConnectWallet from './ConnectWallet.jsx';
+import CurrencyFrom from './CurrencyFrom.jsx';
+import CurrencyTo from './CurrencyTo.jsx';
+import SwapContainerHead from './SwapContainerHead.jsx';
 
 const SwapBox = () => {
   return (
     <div className='swap'>
       <div className='swap_container'>
-        <div className='swap_container_head'>
-          <div className='swap_container_head--wrap'>
-            <div className='swap_container_head_title'>Swap</div>
-
-            <button className='swap_settings_button'>
-              <BiCog />
-            </button>
-            {/* <div className='swap_settings'></div> */}
-          </div>
-        </div>
-        <div className='swap_currency_from'>
-          <div className='swap_currency_from_wrap'>
-            <input></input>
-            <button>
-              <img></img>
-              <span></span>
-              <BiChevronDown />
-            </button>
-          </div>
-        </div>
+        <SwapContainerHead />
+        <CurrencyFrom />
         <div className='swap_currency_switch'>
           <BiSortAlt2 />
         </div>
-        <div className='swap_currency_to'>
-          <div>
-            <input></input>
-            <button>
-              <button>
-                <img></img>
-                <span></span>
-                <BiChevronDown />
-              </button>
-            </button>
-          </div>
-        </div>
-        <div className='swap_connect_wallet'>
-          <button>Connect Wallet</button>
-        </div>
+        <CurrencyTo />
+        <ConnectWallet />
       </div>
     </div>
   );
