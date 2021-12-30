1b2e307422066cd4fe8aaf49dacb12701b879eb8
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-30 14:46:42 -0500

    Wrap React.DOM with MoralisProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 require('file-loader?name=[name].[ext]!../public/index.html');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.scss';
+// import { MoralisProvider } from 'react-moralis';
 
 const appElement = document.getElementById('app');
 
 ReactDOM.render(<App />, appElement);
+// (
+//   <MoralisProvider
+//     appId='jgwQ1t84XpIgPoumdVDyM9bP1y6W9FTIsJ0JmXd0'
+//     serverUrl='https://vwbdrwbl9sy0.usemoralis.com:2053/server'
+//   >
+//     <App />
+//   </MoralisProvider>,
+//   appElement
+// );
